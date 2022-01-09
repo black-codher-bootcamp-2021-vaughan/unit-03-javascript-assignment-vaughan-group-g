7b1a97e0f6d1e7f35bf170f0c443374e74d69609
tjtavia5924 <taviathompson@hotmail.co.uk>
2022-01-09 19:44:31 +0000

    'just testing'

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import {dates} from "./data.js"
+console.log(dates);
+
+const listContainer = document.querySelector ("#timeline-item");
+
+const title = document.createElement("h2");
+
+function fun1 (){
+    var x = document.getElementsByArray("div")
+};

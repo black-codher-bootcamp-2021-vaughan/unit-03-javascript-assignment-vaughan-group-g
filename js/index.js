58eb518d208d9f96ed1d1189582f9883eca7d10e
muna <munaa98@gmail.com>
2022-01-11 21:46:45 +0000

    div,h2 and span created

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
 import {dates} from "./data.js";
 console.log(dates);
 
-// const date = document.createElement("div");
-// date.textContent = date.title;
-// list.appendChild(date);
 
-hi everyone!!
+const timelineItem = document.createElement("div");
+const text = document.createTextNode("This is the timeline-item");
+timelineItem.appendChild(text);
+document.body.appendChild(timelineItem);
+timelineItem.className = "timeline-item";
+
+const timelineItemTitle = document.createElement("h2");
+const title = document.createTextNode("This is the timeline-item-title");
+timelineItemTitle.appendChild(title);
+timelineItem.appendChild(timelineItemTitle);
+timelineItemTitle.className = "timeline-item-Title";
+
+const timelineItemDate = document.createElement("span");
+const date1 = document.createTextNode("This is the timeline-item-date");
+timelineItemDate.appendChild(date1);
+timelineItem.appendChild(timelineItemDate);
+timelineItemDate.className = "timeline-item-Date";

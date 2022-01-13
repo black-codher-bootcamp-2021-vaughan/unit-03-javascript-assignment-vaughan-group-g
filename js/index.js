f48ff3f256f255fe111ae9e571470d9cca00e99a
Hamda Adan <hamdamadan@gmail.com>
2022-01-13 20:07:24 +0000

    Maps over the dates array Co-authored-by: Tavia Thompson <taviathompson@hotmail.co.uk> Co-authored-by: Muna <munaa97@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
 import {dates} from "./data.js";
 console.log(dates);
 
-
-const timelineItem = document.createElement("div");
-const text = document.createTextNode("This is the timeline-item");
-timelineItem.appendChild(text);
-document.body.appendChild(timelineItem);
+ dates.map((date) => {
+    const timelineItem = document.createElement("div");
+    const timelineItemTitle = document.createElement("h2");
+    const title = document.createTextNode(date.title);
+    timelineItemTitle.appendChild(title);
+    timelineItem.appendChild(timelineItemTitle);
+    timelineItemTitle.className = "timeline-item-Title";
+    
+    const timelineItemDate = document.createElement("span");
+    const date1 = document.createTextNode(date.date);
+    timelineItemDate.appendChild(date1);
+    timelineItem.appendChild(timelineItemDate);
+    timelineItemDate.className = "timeline-item-Date"; 
+    document.body.appendChild(timelineItem);
 timelineItem.className = "timeline-item";
+ }); 
 
-const timelineItemTitle = document.createElement("h2");
-const title = document.createTextNode("This is the timeline-item-title");
-timelineItemTitle.appendChild(title);
-timelineItem.appendChild(timelineItemTitle);
-timelineItemTitle.className = "timeline-item-Title";
 
-const timelineItemDate = document.createElement("span");
-const date1 = document.createTextNode("This is the timeline-item-date");
-timelineItemDate.appendChild(date1);
-timelineItem.appendChild(timelineItemDate);
-timelineItemDate.className = "timeline-item-Date";
+

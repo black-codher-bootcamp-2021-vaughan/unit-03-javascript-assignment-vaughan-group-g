6c52c711ff86cda8add6f617e3de6a10f227b383
muna <munaa98@gmail.com>
2022-01-15 18:15:10 +0000

    time line summary and time line more info and button added

--- js/index.js
+++ js/index.js
@@ -18,7 +18,48 @@ console.log(dates);
     timelineItemDate.className = "timeline-item-Date"; 
     document.body.appendChild(timelineItem);
 timelineItem.className = "timeline-item";
+
+
+    const timelineItemSummary = document.createElement("summary");
+    const summary1 = document.createTextNode(date.summary);
+    timelineItemSummary.appendChild(summary1);
+    timelineItem.appendChild(timelineItemSummary);
+    timelineItemSummary.className = "timeline-item-Summary"; 
+    document.body.appendChild(timelineItem);
+
+    const button = document.createElement("button");
+    const text = document.createTextNode("Click to see the full information");
+    button.appendChild(text);
+    document.body.appendChild(button);
+
+    const timelineItemMoreInfo = document.createElement("div");
+    timelineItemMoreInfo.className = "timeline-item-more-info";
+    timelineItem.appendChild(timelineItemMoreInfo);
+    const ModalContent = document.createElement("span");
+    ModalContent.className = "close";
+    timelineItemMoreInfo.appendChild(ModalContent);
+    const modalP = document.createElement("p");
+    modalP.className = "text inside the modal";
+    timelineItemMoreInfo.appendChild(modalP);
+    
+
+
+
+
+    // button.onclick = function() {
+    //     myModal.style.display = "block";
+    //  }
+
+    //  .onclick = function(){
+    //     modal.style.display = "none"
+    //   }
+
+    //   window.onclick = function(e){
+    //     if(e.target == modal){
+    //       modal.style.display = "none"
+    //     }
+    //   }
+    // document.body.appendChild(myModal);
+
+
  }); 
-
-
-

bd872419d87c7d7f7ffac1b07122e3283fe9e821
muna <munaa98@gmail.com>
2022-01-21 19:14:23 +0000

    modal created and info added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+//make buttons the same class
+// figure out how to make the modal disappear
+// content in the modal
 
 
 import {dates} from "./data.js";
@@ -5,6 +8,8 @@ console.log(dates);
 
  dates.map((date) => {
     const timelineItem = document.createElement("div");
+    document.body.appendChild(timelineItem);
+    timelineItem.className = "timeline-item";
     const timelineItemTitle = document.createElement("h2");
     const title = document.createTextNode(date.title);
     timelineItemTitle.appendChild(title);
@@ -16,8 +21,9 @@ console.log(dates);
     timelineItemDate.appendChild(date1);
     timelineItem.appendChild(timelineItemDate);
     timelineItemDate.className = "timeline-item-Date"; 
-    document.body.appendChild(timelineItem);
-timelineItem.className = "timeline-item";
+    
+
+
 
 
     const timelineItemSummary = document.createElement("summary");
@@ -27,39 +33,54 @@ timelineItem.className = "timeline-item";
     timelineItemSummary.className = "timeline-item-Summary"; 
     document.body.appendChild(timelineItem);
 
-    const button = document.createElement("button");
+
+
+    const buttonOpen = document.createElement("button");
     const text = document.createTextNode("Click to see the full information");
-    button.appendChild(text);
-    document.body.appendChild(button);
+    buttonOpen.appendChild(text);
+    document.body.appendChild(buttonOpen);
+    buttonOpen.className = "open"; 
+
 
     const timelineItemMoreInfo = document.createElement("div");
     timelineItemMoreInfo.className = "timeline-item-more-info";
     timelineItem.appendChild(timelineItemMoreInfo);
-    const ModalContent = document.createElement("span");
-    ModalContent.className = "close";
+
+    const ModalContent = document.createElement("div");
+    ModalContent.className = "modal";
     timelineItemMoreInfo.appendChild(ModalContent);
+
     const modalP = document.createElement("p");
-    modalP.className = "text inside the modal";
-    timelineItemMoreInfo.appendChild(modalP);
+    const modalIptext = document.createTextNode([date.title , date.date, date.summary]);
+    modalP.appendChild(modalIptext);
+    modalP.className = "text-inside-the-modal";
+    document.body.appendChild(modalP);
+    ModalContent.appendChild(modalP);
+
+    
+    const buttonClose = document.createElement("button");
+    const closeMeText = document.createTextNode("Close ");
+    buttonClose.appendChild(closeMeText);
+    document.body.appendChild(buttonClose);
+    buttonClose.className = "close"; 
+    ModalContent.appendChild(buttonClose);
+    
     
 
 
 
+    buttonOpen.onclick = function() {
+        timelineItemMoreInfo.style.display = "block";
+     }
 
-    // button.onclick = function() {
-    //     myModal.style.display = "block";
-    //  }
+     buttonClose.onclick = function() {
+        timelineItemMoreInfo.style.display = "none";
+      }
 
-    //  .onclick = function(){
-    //     modal.style.display = "none"
-    //   }
-
-    //   window.onclick = function(e){
-    //     if(e.target == modal){
-    //       modal.style.display = "none"
-    //     }
-    //   }
-    // document.body.appendChild(myModal);
-
-
+      window.onclick = function(event) {
+        if (event.target == timelineItemMoreInfo) {
+          timelineItemMoreInfo.style.display = "none"
+        }
+      }
+  
  }); 
